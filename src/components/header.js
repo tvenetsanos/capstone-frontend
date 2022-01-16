38ab7d707f40a3c6c052b2a16b17b95551a1819a
Tyler <tyler_venetsanos@msn.com>
2022-01-16 12:29:00 -0500

    dropdown

--- src/components/header.js
+++ src/components/header.js
@@ -8,6 +8,11 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from "react-router-dom";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ForumIcon from '@material-ui/icons/Forum';
 
 class Header extends Component {
   constructor(props) {
@@ -41,7 +46,23 @@ class Header extends Component {
           <div style={{marginLeft: '60rem'}}>
             {this.props.isLoggedIn && <Button onClick={this.logOut}>Logout</Button>}
             {!this.props.isLoggedIn && <Button href="/login">Sign In</Button>}
-          </div>
+            </div>
+            <div>
+              <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                  <React.Fragment>
+                    <Button variant="contained" {...bindTrigger(popupState)}>
+                      <AccountCircleIcon/>
+                    </Button>
+              <Menu {...bindMenu(popupState)}>
+                <MenuItem onClick={popupState.close}>Profile</MenuItem>
+                <MenuItem onClick={popupState.close}>My account</MenuItem>
+                <MenuItem onClick={popupState.close}>Logout</MenuItem>
+              </Menu>
+            </React.Fragment>
+                )}
+              </PopupState>
+            </div>
         </Toolbar>
       </AppBar>
         {this.state.redirect && <Redirect to="/" />}
